230d9326a6ba90a364f7dc87078937b516a3fcff
1Kuso4ek1 <dimasalov2006@gmail.com>
2024-08-20 20:47:49 +0300

    Some improvements over the page

--- html/APICall.js
+++ html/APICall.js
@@ -1,8 +1,15 @@
 const apiAddress = "http://localhost:8080/api";
+const apiKeyAddress = "http://localhost:8080/auth";
 
 const usernameInput = document.getElementById("username");
 const apiCallButton = document.getElementById("apiCallButton");
 const apiResponse = document.getElementById("apiResponse");
+
+const usernameKeyInput = document.getElementById("usernameKey");
+const apiKeyInput = document.getElementById("apiKey");
+const registerButton = document.getElementById("registerButton");
+const getUserButton = document.getElementById("getUserButton");
+const apiKeyResponse = document.getElementById("apiKeyResponse");
 
 function APICall()
 {
@@ -22,4 +29,49 @@ function APICall()
         });
 }
 
+function Register()
+{
+    fetch(apiKeyAddress + "?user=" + usernameKeyInput.value, { method: "POST" })
+        .then(response => response.json())
+        .then(data => {
+            apiKeyResponse.innerHTML = `
+                <p>API Response:</p>
+                <pre>${ JSON.stringify(data, null, 2) }</pre>
+            `;
+        })
+        .catch(error => {
+            apiKeyResponse.innerHTML = `
+                <p>Error fetching data:</p>
+                <pre>${ error.message }</pre>
+            `;
+        });
+}
+
+function GetUserInfo()
+{
+    fetch(apiKeyAddress,
+    {
+        headers:
+        {
+            Authorization: "Bearer " + apiKeyInput.value
+        },
+        method: "GET"
+    })
+    .then(response => response.json())
+    .then(data => {
+        apiKeyResponse.innerHTML = `
+            <p>API Response:</p>
+            <pre>${ JSON.stringify(data, null, 2) }</pre>
+        `;
+    })
+    .catch(error => {
+        apiKeyResponse.innerHTML = `
+            <p>Error fetching data:</p>
+            <pre>${ error.message }</pre>
+        `;
+    });
+}
+
 apiCallButton.addEventListener("click", APICall);
+registerButton.addEventListener("click", Register);
+getUserButton.addEventListener("click", GetUserInfo);
